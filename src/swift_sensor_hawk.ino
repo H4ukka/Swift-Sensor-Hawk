72ec68a488478db806a6dd64dd9966efd14acbe2
A.E.K Laine <laine.antti.e@gmail.com>
2015-04-18 20:52:53 +0300

    Temperature sensing adjustments
    Measuring tempearatures is almost working, it's a bit unreliable.

--- src/swift_sensor_hawk.ino
+++ src/swift_sensor_hawk.ino
@@ -18,6 +18,8 @@
 */
 
 #include <UTFT.h>
+#include <SPI.h>
+#include <SD.h>
 
 #include "colors.h"
 #include "ScreenDrawer.h"
@@ -32,11 +34,15 @@ ScreenDrawer Screen0;
 ScreenDrawer Screen1;
 ScreenDrawer Screen2;
 
+const int chipSelect = 53;
+
 void setup () {
 
-	analogReference(INTERNAL2V56);
+    //analogReference(INTERNAL2V56);
 
-	LCD0_.InitLCD();
+    Serial.begin(9600);
+
+    LCD0_.InitLCD();
 
     SensorMaster.addChannel(3);
     SensorMaster.addChannel(4);
@@ -45,15 +51,25 @@ void setup () {
     Screen0.draw_panel ("MAIN_GRAPH");
 
     Screen0.set_color(DEFAULT_RED);
-	// Screen1.set_color(DEFAULT_GREEN);
-	// Screen2.set_color(DEFAULT_YELLOW);
+    // Screen1.set_color(DEFAULT_GREEN);
+    // Screen2.set_color(DEFAULT_YELLOW);
+    //Serial.print("Initializing SD card...");
+
+    // // see if the card is present and can be initialized:
+    // if (!SD.begin(chipSelect)) {
+    //     Serial.println("Card failed, or not present");
+    //     // don't do anything more:
+    //     return;
+    // }
+    // Serial.println("card initialized.");
 }
 
 void loop () {
 
     // Screen0.draw_graph(map(SensorMaster.measure(0),0,1023,118,0));
-    Screen0.draw_graph(SensorMaster.measure(0)-434);
-    Screen0.draw_sensor_box (20,140,SensorMaster.measure(0));
+    Screen0.draw_graph(120-(SensorMaster.measure(0)-380));
+    Screen0.draw_sensor_box (20,140,(502 - SensorMaster.measure(0))*1.494);
+    Screen0.draw_sensor_box (160,140,SensorMaster.measure(0));
     Screen0.step_forward();
 
     // Screen1.draw_graph(map(SensorMaster.measure(0),0,1023,0,118));
@@ -63,4 +79,22 @@ void loop () {
     // Screen2.draw_graph(map(SensorMaster.measure(2),0,1023,0,118));
     // Screen2.draw_sensor_box (260,140,map(SensorMaster.measure(2),0,1023,0,118));
     // Screen2.step_forward();
+
+    // String dataString = (String)SensorMaster.measure(0);
+
+    // // open the file. note that only one file can be open at a time,
+    // // so you have to close this one before opening another.
+    // File dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+    // // if the file is available, write to it:
+    // if (dataFile) {
+    //     dataFile.println(dataString);
+    //     dataFile.close();
+    //     // print to the serial port too:
+    //     Serial.println(dataString);
+    // }
+    // // if the file isn't open, pop up an error:
+    // else {
+    //     Serial.println("error opening datalog.txt");
+    // }
 }
